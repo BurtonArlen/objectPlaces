e26a8b9bf13a8def39479f86137e162fa0b5b604
Arlen Burton and Ryan Carlile <ryan@>
2016-04-04 14:00:06 -0700

    applies functional user logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-function place(country,city,landmark,time,note) {
+function Place(country,city,landmark,time,note) {
   this.country = country;
   this.city = city;
   this.landmark = landmark;
@@ -6,7 +6,13 @@ function place(country,city,landmark,time,note) {
   this.note = note;
 }
 
-place.prototype.fullName = function() {
+function PlaceName(city,country) {
+  this.city = city;
+  this.country = country;
+
+}
+
+Place.prototype.fullName = function() {
   return this.city + ", " + this.country;
 }
 
@@ -22,10 +28,26 @@ $(document).ready(function() {
     var newLandmark = $("input#new-landmark").val();
     var newTime = $("input#new-time").val();
     var newNote = $("input#new-note").val();
-    var newPlace = new place(newCountry,newCity, newLandmark, newTime, newNote, newPlace);
-    console.log(newPlace);
+    var newPlace = new Place(newCountry, newCity, newLandmark, newTime, newNote);
+    $("#show-places").show();
+    $('#show-places h2').text(newPlace.fullName());
+    $(".new-country").text(newPlace.country);
+    $(".new-city").text(newPlace.city);
+    $(".new-landmark").text(newPlace.landmark);
+    $(".new-time").text(newPlace.time);
+    $(".new-note").text(newPlace.note);
 
 
-    $("ul#places").append("<li><span class='Place'>" + newPlace + "</span></li>");
-  });
-});
+
+
+
+        // $("ul#places").append("<li>" + new + "</li>");
+      });
+//     var newPlaceName = new PlaceName(newCity, newCountry)
+// console.log(newPlace);
+//     debugger;
+//  $("ul#places").append("<li><span class=''" + newPlaceName + "</li>");
+
+ });
+
+// });
